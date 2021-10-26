7ecb69b666a9ae7610188a1b8cae2a05dac9fd3b
mycarlos26 <mycarlos_26@hotmail.com>
2021-10-26 15:18:39 -0400

    versión actualizada de library

--- testsolidity/PancakeLibrary.sol
+++ testsolidity/PancakeLibrary.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.6.6;
 
-import "../testsolidity/IPancakePair.sol";
+import "./IPancakePair.sol";
 
-import "../testsolidity/SafeMath.sol";
+import "./SafeMath.sol";
 
 library PancakeLibrary {
     using SafeMath for uint;
